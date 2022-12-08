f324999235ad1dde69bffd5067cc09c0d0827c9a
Jimmy Zhang <jiemingz@nvidia.com>
2022-12-08 13:26:36 -0800

    save
    Signed-off-by: Jimmy Zhang <jiemingz@nvidia.com>

--- nemo/collections/nlp/data/language_modeling/megatron/gpt_prompt_learning_dataset.py
+++ nemo/collections/nlp/data/language_modeling/megatron/gpt_prompt_learning_dataset.py
@@ -123,7 +123,7 @@ class GPTPromptLearningDataset(Dataset):
         skipped = 0
 
         for json_line in tqdm(dataset):
-            if len(self.examples) > 8000:
+            if len(self.examples) > 2000:
                 break
             # Read example dict or load the information for a single example from .json file
             if type(json_line) == dict:
