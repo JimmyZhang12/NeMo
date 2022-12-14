182ed502ab9083e3553be7cac6a8ebfd627d225b
Jimmy Zhang <jieminngz@nvidia.coom>
2022-12-13 16:37:47 -0800

    working
    Signed-off-by: Jimmy Zhang <jieminngz@nvidia.coom>

--- nemo/collections/nlp/models/language_modeling/megatron_base_model.py
+++ nemo/collections/nlp/models/language_modeling/megatron_base_model.py
@@ -140,7 +140,8 @@ class MegatronBaseModel(NLPModel):
             # Apex Persistent layer norm is supported from Nvidia PyTorch container v21.11
             # This only depends on Apex version?
             if NVIDIA_TORCH_MAJOR < 21 or (NVIDIA_TORCH_MAJOR == 21 and NVIDIA_TORCH_MINOR < 11):
-                self.cfg.persist_layer_norm = False
+                # self.cfg.persist_layer_norm = False
+                pass
 
             # NVFUSER available starting with 21.11
             if NVIDIA_TORCH_MAJOR >= 21 or (NVIDIA_TORCH_MAJOR == 21 and NVIDIA_TORCH_MINOR >= 11):
